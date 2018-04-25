ff3599c879a8f297e6fdd7d46953a6cd34118744
Rails Udemy Course <railsudenycourse@gmail.com>
2018-04-25 02:13:45 +0000

    add partials and delete function

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -38,6 +38,12 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
     end
     
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path
+        flash[:notice] = "Article was sucessfully deleted"
+    end
     
     private
     def article_params
